ce71147465d04194e29e94ec3c0f2bcc6d7b0095
Gleb <50069183+Gleebo@users.noreply.github.com>
2019-09-04 11:10:31 +1000

    firebase config added
    Firebase pacjage in package.json

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/app";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyA7vQPCa3-TE8Ve-QiAH9l80aYir7oJkZU",
+  authDomain: "kids-islands.firebaseapp.com",
+  databaseURL: "https://kids-islands.firebaseio.com",
+  projectId: "kids-islands",
+  storageBucket: "",
+  messagingSenderId: "653194778308",
+  appId: "1:653194778308:web:d168438b672124cd"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
