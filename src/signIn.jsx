da62de1caee1bdf3e08f2bb961372c69382abbea
297768427 <37395261+297768427@users.noreply.github.com>
2019-10-01 16:51:25 +1000

    sign in/up pages
    wait for implementaion

--- src/signIn.jsx
+++ src/signIn.jsx
@@ -1,54 +1,13 @@
-import React from "react";
+import React, { Component } from "react";
 
-const SignIn = () => {
-  return (
-    <div className="row justify-content-center align-items-center ">
-      <div className="row">
-        <div className="span12">
-          <form className="form-horizontal" action="" method="POST">
-            <fieldset>
-              <div id="legend">
-                <legend className="">Log In</legend>
-              </div>
-              <div className="control-group">
-                <label className="control-label" for="username">
-                  Account
-                </label>
-                <div className="controls">
-                  <input
-                    type="text"
-                    id="username"
-                    name="username"
-                    placeholder="Email or School Name"
-                    className="input-xlarge"
-                  />
-                </div>
-              </div>
-              <div className="control-group">
-                <label className="control-label" for="password">
-                  Password
-                </label>
-                <div className="controls">
-                  <input
-                    type="password"
-                    id="password"
-                    name="password"
-                    placeholder=""
-                    className="input-xlarge"
-                  />
-                </div>
-              </div>
-              <div className="control-group">
-                <div className="controls">
-                  <button className="btn btn-success m-2">Log In</button>
-                </div>
-              </div>
-            </fieldset>
-          </form>
-        </div>
-      </div>
-    </div>
-  );
-};
+class SignIn extends Component {
+  state = {
+    account: "",
+    password: ""
+  };
+  render() {
+    return <div></div>;
+  }
+}
 
 export default SignIn;
--- src/signUp.jsx
+++ src/signUp.jsx
@@ -1,86 +1,9 @@
 import React, { Component } from "react";
-
-const SignUp = () => {
-  return (
-    <div className="row justify-content-center align-items-center ">
-      <div className="row">
-        <div className="span12">
-          <form className="form-horizontal" action="" method="POST">
-            <fieldset>
-              <div id="legend">
-                <legend className="">Sign Up</legend>
-              </div>
-              <div className="control-group">
-                <label className="control-label" for="username">
-                  Eamil
-                </label>
-                <div className="controls">
-                  <input
-                    type="text"
-                    id="username"
-                    name="username"
-                    placeholder=""
-                    className="input-xlarge"
-                  />
-                </div>
-              </div>
-
-              <div className="control-group">
-                <label className="control-label" for="username">
-                  School Name
-                </label>
-                <div className="controls">
-                  <input
-                    type="text"
-                    id="username"
-                    name="username"
-                    placeholder=""
-                    className="input-xlarge"
-                  />
-                </div>
-              </div>
-
-              <div className="control-group">
-                <label className="control-label" for="password">
-                  Password
-                </label>
-                <div className="controls">
-                  <input
-                    type="password"
-                    id="password"
-                    name="password"
-                    placeholder=""
-                    className="input-xlarge"
-                  />
-                </div>
-              </div>
-
-              <div className="control-group">
-                <label className="control-label" for="password">
-                  Confirm Password
-                </label>
-                <div className="controls">
-                  <input
-                    type="password"
-                    id="passwordC"
-                    name="passwordC"
-                    placeholder=""
-                    className="input-xlarge"
-                  />
-                </div>
-              </div>
-
-              <div className="control-group">
-                <div className="controls">
-                  <button className="btn btn-success m-2">Sgin Up</button>
-                </div>
-              </div>
-            </fieldset>
-          </form>
-        </div>
-      </div>
-    </div>
-  );
-};
+class SignUp extends Component {
+  state = {};
+  render() {
+    return <div></div>;
+  }
+}
 
 export default SignUp;
