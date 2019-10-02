df8a5986a1406770812602f5ff2edaf0439ab99d
297768427 <37395261+297768427@users.noreply.github.com>
2019-10-02 12:30:13 +1000

    sign in/up front end complete
    these two pages are so far not connected to the backend

--- src/signIn.jsx
+++ src/signIn.jsx
@@ -2,15 +2,34 @@ import React, { Component } from "react";
 
 class SignIn extends Component {
   state = {
-    account: "",
-    password: ""
+    account: {
+      username: "",
+      password: ""
+    }
   };
 
   handleSubmit = e => {
     e.preventDefault(); // prevent pre-default function of <form> element
+
+    if (
+      this.state.account.username === "" ||
+      this.state.account.password === ""
+    ) {
+      window.alert("sign in information is not complete");
+    } else {
+      window.open("/", "_self");
+    }
+  };
+
+  handleChange = e => {
+    const account = { ...this.state.account };
+    account[e.currentTarget.name] = e.currentTarget.value;
+    this.setState({ account });
   };
 
   render() {
+    const { account } = this.state;
+
     return (
       <div className="d-flex justify-content-center">
         <div style={{ marginTop: 100, width: 400 }}>
@@ -20,11 +39,25 @@ class SignIn extends Component {
           <form onSubmit={this.handleSubmit}>
             <div className="form-group">
               <label htmlFor="theUsername">Username</label>
-              <input id="theUsername" type="text" className="form-control" />
+              <input
+                value={account.username}
+                name="username"
+                onChange={this.handleChange}
+                id="theUsername"
+                type="text"
+                className="form-control"
+              />
             </div>
             <div className="form-group">
               <label htmlFor="thePassword">Password</label>
-              <input id="thePassword" type="text" className="form-control" />
+              <input
+                value={account.password}
+                name="password"
+                onChange={this.handleChange}
+                id="thePassword"
+                type="password"
+                className="form-control"
+              />
             </div>
             <button className="btn btn-primary">Sign In</button>
           </form>
